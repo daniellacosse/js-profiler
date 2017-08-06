e6123034efe2dc18522eeac11729f5d143b67bef
Gang Qiu <gangqiu0@gmail.com>
2017-08-06 19:27:16 -0400

    adds CodeTabNav

---
+++ src/code-tabs-nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavItem, NavLink} from 'reactstrap';
+import classnames from 'classnames';
+
+
+export default class CodeTabNav extends React.Component {
+  // nest this under <Nav tabs> bootstrap element
+  render() {
+    return (
+      <NavItem>
+        <NavLink
+          className={classnames({ active: this.props.active })}
+          onClick={() => { this.props.onClick()}}
+        >
+          {this.props.title}
+        </NavLink>
+      </NavItem>
+    );
+  }
+}
--- src/code-tabs.js
+++ src/code-tabs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
 import CodeMirror from 'react-codemirror';
 import classnames from 'classnames';
+import CodeTabNav from './code-tabs-nav';
 
 export default class CodeTabs extends React.Component {
   constructor(props) {
@@ -28,14 +29,6 @@ export default class CodeTabs extends React.Component {
         <Nav tabs>
           <NavItem>
             <NavLink
-              className={classnames({ active: this.state.activeTab === '1' })}
-              onClick={() => { this.toggle('1'); }}
-            >
-              Function 1
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
               className={classnames({ active: this.state.activeTab === '2' })}
               onClick={() => { this.toggle('2'); }}
             >
@@ -44,13 +37,6 @@ export default class CodeTabs extends React.Component {
           </NavItem>
         </Nav>
         <TabContent activeTab={this.state.activeTab}>
-          <TabPane tabId="1">
-            <Row>
-              <Col sm="12">
-                <CodeMirror/>
-              </Col>
-            </Row>
-          </TabPane>
           <TabPane tabId="2">
             <Row>
               <Col sm="12">
