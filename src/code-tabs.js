251f7b3ba51e0917379ce5f1eca1d60755e82208
Gang Qiu <gangqiu0@gmail.com>
2017-08-06 20:15:03 -0400

    able to add new tabs

--- src/code-tabs.js
+++ src/code-tabs.js
@@ -22,6 +22,16 @@ export default class CodeTabs extends React.Component {
     }
   }
 
+  addNewTab() {
+    let lastTab = parseInt(this.state.tabs[this.state.tabs.length - 1], 10);
+    let newTab = String(lastTab + 1)
+    let newTabs = this.state.tabs.concat([newTab]);
+    this.setState({
+      tabs: newTabs,
+      activeTab: newTab
+    })
+  }
+
   render() {
     return (
       <div id="code-area">
@@ -31,9 +41,15 @@ export default class CodeTabs extends React.Component {
               key={i}
               title={`Function ${tabId}`}
               active={this.state.activeTab === tabId}
-              onClick={() => {this.toggle(tabId)}}>
+              onClick={() => this.toggle(tabId)}>
             </CodeTabNav>
           })}
+          <CodeTabNav 
+            title="+"
+            active={false}
+            onClick={() => this.addNewTab()}
+          >
+          </CodeTabNav>
         </Nav>
         <TabContent activeTab={this.state.activeTab}>
           {this.state.tabs.map((tabId, i) => <CodeTabPane key={i} id={tabId}></CodeTabPane>)}
